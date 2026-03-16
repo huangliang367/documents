e054e4799dfd4791abdef0cf89a06341c8e7f999
Huang Liang <584782297@qq.com>
2026-03-16 20:58:27 +0800

    add github workflow

--- envsetup.sh
+++ envsetup.sh
@@ -10,6 +10,7 @@ if [ ! -d "$VENV_DIR" ]; then
     python -m venv .venv
     source .venv/bin/activate
     "$VENV_DIR/bin/pip" install -r "$REQ_TEXT"
+    pip install sphinx-autobuild
 else
     echo "Virtual environment already exists."
 fi
